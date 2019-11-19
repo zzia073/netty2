797d9693a666df8f2335ada88f206debad1a95ec
qinpengfei <625965155@qq.com>
2019-11-19 15:48:46 +0800

    protobuf 学习

--- src/main/java/com/study/base/protobuf/ProtobufServer.java
+++ src/main/java/com/study/base/protobuf/ProtobufServer.java
@@ -14,9 +14,10 @@ import io.netty.handler.codec.protobuf.ProtobufDecoder;
 /**
  * @author ：fei
  * @date ：Created in 2019/11/18 0018 17:42
+ *
  * 服务端要和客户端共用.proto文件或生成的文件，解决方案
- * 1.用Git作为版本控制系统：
- *  git submodule: git仓库里的一个仓库
+ * 1. 用Git作为版本控制系统：
+ *  git submodule: git仓库里的一个仓库 让本地项目引用外部项目，还是两个项目，两个仓库
  *
  *  ServerProject: 主工程服务端
  *
@@ -29,6 +30,8 @@ import io.netty.handler.codec.protobuf.ProtobufDecoder;
  *      develop:
  *      test:
  *      master:
+ *  2. git subtree 将Protobuf-Java项目merge到主工程仓库，合并为一个项目（推荐）
+ *  3. nexus 把生成的java文件做成jar包然后两个项目去依赖
  */
 public class ProtobufServer {
     public static void main(String[] args) throws Exception {
