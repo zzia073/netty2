8861ff2213f3cdee375afd92ac4ac9ed96555af1
qinpengfei <625965155@qq.com>
2019-11-20 16:11:17 +0800

    thrift学习

---
+++ src/main/java/com/study/base/thrift/PersonServiceImpl.java
@@ -0,0 +1,8 @@
+package com.study.base.thrift;
+
+/**
+ * @author ：fei
+ * @date ：Created in 2019/11/19 0019 17:42
+ */
+public class PersonServiceImpl {
+}
---
+++ src/main/java/com/study/base/thrift/ThriftClient.java
@@ -0,0 +1,8 @@
+package com.study.base.thrift;
+
+/**
+ * @author ：fei
+ * @date ：Created in 2019/11/20 0020 09:04
+ */
+public class ThriftClient {
+}
